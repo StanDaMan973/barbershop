9932bd8ce43f676fb380bdab5766cd46cc1a68a0
StanDaMan973 <stanley.espaillat@gmail.com>
2020-01-07 17:36:57 -0500

    almost done with tab table fixing a bug

--- js/barber.js
+++ js/barber.js
@@ -1,21 +1,10 @@
-// I am creating a testimonial carousel feature 
-
-//what do i need
-// i need to select all of my carousel items
-// i need to select all of my indicators for the carousel
-// i need to create a variable and set it to 0 to track all of my slides indexes
-// i need to create a variable and set it to true 
-
-// i need to create variable and add a event listener to both the left and right buttons 
-// i need to create a event listener for my indictors. they need to update according to the index of the current slide
-
+// Testimonial Carousel 
 var items = document.querySelectorAll('.Testimonials-carousel .item');
 var dots = document.querySelectorAll('.carousel-indicators-tm li');
 var currentItem = 0;
 var isEnabled = true;
 
 
-console.log(items, dots)
 
 function changeCurrentItem(n) {
 	currentItem = (n + items.length) % items.length;
@@ -46,11 +35,11 @@ function goToItem(n) {
 }
 
 function hideItem(direction) {
- 
+
 	isEnabled = false;
 	items[currentItem].classList.add(direction);
 	dots[currentItem].classList.remove('active');
-	items[currentItem].addEventListener('animationend', function() {
+	items[currentItem].addEventListener('animationend', function () {
 		this.classList.remove('active', direction);
 	});
 }
@@ -59,29 +48,66 @@ function showItem(direction) {
 
 	items[currentItem].classList.add('next', direction);
 	dots[currentItem].classList.add('active');
-	items[currentItem].addEventListener('animationend', function() {
+	items[currentItem].addEventListener('animationend', function () {
 		this.classList.remove('next', direction);
 		this.classList.add('active');
 		isEnabled = true;
 	});
 }
 
-document.querySelector('.carousel-control.left').addEventListener('click', function() {
+document.querySelector('.carousel-control.left').addEventListener('click', function () {
 	if (isEnabled) {
 		previousItem(currentItem);
 	}
 });
 
-document.querySelector('.carousel-control.right').addEventListener('click', function() {
+document.querySelector('.carousel-control.right').addEventListener('click', function () {
 	if (isEnabled) {
 		nextItem(currentItem);
 	}
 });
 
-document.querySelector('.carousel-indicators-tm').addEventListener('click', function(e) {
+document.querySelector('.carousel-indicators-tm').addEventListener('click', function (e) {
 	var target = [].slice.call(e.target.parentNode.children).indexOf(e.target);
 	if (target !== currentItem && target < dots.length) {
 		goToItem(target);
 	}
 });
+
+
+// problem: I need to create a function that will display my content 
+// when i click on a tab
+
+//steps
+// i need to get all the tabs and save them to a variole - $
+// i need to get all the table items and save them to a variable -$
+// i need to add a click event to all the elements - $
+// i need to hide all the table elements
+// i need to be able to remove the active class from the elements
+// i need to add the active class to the tabs when clicked 
+// i need to able to add the active class to tableitems and display the table
+
+
+let tabLinks = document.querySelectorAll('[data-tab-target]');
+let tabItems = document.querySelectorAll('[data-tab-content]');
+
+
+
+
+tabLinks.forEach(function(tab){
+	tab.addEventListener('click', function() {
+		tab.classList.add('active');	
+		for (let i = 0; i < tab.length; i++) {
+			tab.classList.remove('active');
+			
+		}
+
+		let target = document.querySelector(tab.dataset.tabTarget)
+		tabItems.forEach(function(tabItems) {
+			tabItems.classList.remove('active');
+		});
+		target.classList.add('active');
+	})
+});
+
 
